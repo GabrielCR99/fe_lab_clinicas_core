615eb7007bac399d446774364a75376eaab08e97
Roveri <gabriel.roveri7@gmail.com>
2025-05-30 23:28:05 -0300

    deps: update Flutter SDK and package versions

--- lib/src/widgets/lab_clinicas_app_bar.dart
+++ lib/src/widgets/lab_clinicas_app_bar.dart
@@ -8,22 +8,22 @@ base class LabClinicasAppBar extends AppBar {
     super.backgroundColor = Colors.transparent,
     super.actions,
   }) : super(
-          flexibleSpace: Container(
-            alignment: Alignment.centerLeft,
-            decoration: BoxDecoration(
-              color: Colors.white,
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black.withOpacity(0.5),
-                  offset: const Offset(0, 1),
-                  blurRadius: 4,
-                ),
-              ],
-            ),
-            child: Padding(
-              padding: const EdgeInsets.only(left: 64),
-              child: Image.asset('assets/images/logo_horizontal.png'),
-            ),
-          ),
-        );
+         flexibleSpace: Container(
+           alignment: Alignment.centerLeft,
+           decoration: BoxDecoration(
+             color: Colors.white,
+             boxShadow: [
+               BoxShadow(
+                 color: Colors.black.withValues(alpha: 0.5),
+                 offset: const Offset(0, 1),
+                 blurRadius: 4,
+               ),
+             ],
+           ),
+           child: Padding(
+             padding: const EdgeInsets.only(left: 64),
+             child: Image.asset('assets/images/logo_horizontal.png'),
+           ),
+         ),
+       );
 }
